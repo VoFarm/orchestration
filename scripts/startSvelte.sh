33200c66965d40724bd94ef8df02244cff82a0d2
Nikita Trotner <nikita@ttnr.me>
2022-02-19 22:04:43 +0100

    fix build execution

--- scripts/startSvelte.sh
+++ scripts/startSvelte.sh
@@ -2,4 +2,4 @@
 
 cd ../
 git clone https://github.com/VoFarm/trading-actor-interface.git
-cd trading-actor-interface && npm install && npx svelte build
+cd trading-actor-interface && npm install && npm run build
